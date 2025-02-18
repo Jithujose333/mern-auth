5a5140cbbcf8e0b83331171790456d56d2e68a2d
JithuJose <jithujose333@gmail.com>
2025-02-19 01:51:17 +0530

    add redux toolkit

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from "react-router-dom"
+import { loginStart, loginSuccess ,loginFailure } from '../redux/user/userSlice.js'
+import {useDispatch, useSelector} from 'react-redux'
+
+
+
 
 function Login() {
+  const [formData,setFormData] = useState({})
+ const {loading , error} = useSelector((state)=> state.user)
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const handleChange =(e)=>{ 
+    setFormData({...formData, [e.target.id]: e.target.value})
+  }
+  
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    try {
+
+    dispatch(loginStart())
+
+    const res = await fetch('/api/auth/login',{
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData)
+    })
+    
+ 
+    const data = await res.json()
+    
+    if(data.success === false){
+     dispatch(loginFailure(data.message || "Invalid credentials"))
+      return
+    }
+    
+
+    dispatch(loginSuccess(data))
+    navigate('/')
+      
+      
+    } catch (error) {
+      dispatch(loginFailure(error.message|| "something went wrong"))
+ 
+    }
+    
+  } 
+  
   return (
+    <div className='p-3 max-w-lg mx-auto'>
+    <h1 className='text-3xl text-center font-semibold my-7'>Login</h1>
+    <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+   
+      <input type="text" placeholder='Email' id='email'
+      className='bg-slate-100 p-3 rounded-lg' 
+      onChange={handleChange} />
+      <input type="password" placeholder='Password' id='password'
+      className='bg-slate-100 p-3 rounded-lg' 
+      onChange={handleChange} />
+      <button disabled={loading} className='bg-slate-700 text-white p-3 
+      rounded-lg uppercase hover:opacity-95
+      disabled:opacity-80'>{loading?'Loading...':'Login'}</button>
+    </form>
     <div>
-      login
+      <p>Don't have an account</p>
+      <Link to="/signup"> 
+      <span className='text-blue-500'>Sign UP</span>
+      </Link>
+    </div>
+    <p className='text-red-700 mt-5'>{error}</p>
+     
     </div>
   )
 }
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 function Signup() {
   const [formData,setFormData] = useState({})
   const [error,setError] = useState(null)
   const [loading,setLoading] = useState(false)
+  const navigate = useNavigate()
   const handleChange =(e)=>{ 
     setFormData({...formData, [e.target.id]: e.target.value})
   }
@@ -32,7 +33,7 @@ function Signup() {
       return
     }
     
- 
+     navigate('/login')
       
     } catch (error) {
       setLoading(false)
